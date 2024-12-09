25f34c01208d5367bced36c16a24bcbe55b5643a
wayiam <sakshamsingh085@gmail.com>
2024-12-09 14:00:51 +0530

    Add health check route for server status monitoring

---
+++ src/routes/healthCheck.routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import { healthCheck } from '../controllers/healthCheck.controllers.js'
+
+
+const routers = Router()
+
+routers.route("/").get(healthCheck)
+
+export default routers;
