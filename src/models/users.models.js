eb45c599a2188c2310c78779d46c89506f5021a0
wayiam <sakshamsingh085@gmail.com>
2024-12-10 13:32:50 +0530

    Enhance User model with password hashing, token generation, and authentication methods

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jwt";
 
 const userSchema = new Schema({
     username: {
@@ -48,5 +50,41 @@ const userSchema = new Schema({
 }, {
     timestamp: true,
 });
+
+userSchema.pre("save", async function(next) {
+    if (!this.modified("password")) {
+        next();
+    }
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAcessToken = async function() {
+    //short time web token generation
+    return jwt.sign({
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        process.env.ACCESS_TOKEN_EXPIRY
+    );
+};
+
+userSchema.methods.generateRefreshToken = async function() {
+    //short time web token generation
+    return jwt.sign({
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        process.env.REFRESH_TOKEN_EXPIRY
+    );
+};
 
 export const User = mongoose.model("User", userSchema);
