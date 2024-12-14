0e214992869f3e0935508960c97b5896e15218b9
wayiam <sakshamsingh085@gmail.com>
2024-12-14 13:43:08 +0530

    update Cloudinary utility: Add environment variable support and delete functionality

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import env from "dotenv";
+
+env.config();
 
 //Configure cloudinary
 cloudinary.config({
@@ -12,75 +15,26 @@ const uploadOnCloudinary = async(localfilepath) => {
     try {
         if (!localfilepath) return null;
         const response = await cloudinary.uploader.upload(localfilepath, {
-            resourceType: "auto"
-        })
-        console.log(`File uploaded on Cloudinary. FilePath: ${response.url}`)
-            //Once the file is uploaded delete it fom our server
+            resourceType: "auto",
+        });
+        console.log(`File uploaded on Cloudinary. FilePath: ${response.url}`);
+        //Once the file is uploaded delete it fom our server
         fs.unlinkSync(localfilepath);
         return response;
     } catch (e) {
         fs.unlinkSync(localfilepath);
         return null;
     }
-}
+};
 
-export { uploadOnCloudinary }
+const deletedFromCloudinary = async(publicId) => {
+    try {
+        const result = await cloudinary.uploader.destroy(publicId);
+        console.log("Deleted from cloudinary.PublicId: " + publicId);
+    } catch (error) {
+        console.log("Error deleting  from cloudinary", error);
+        return null;
+    }
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-(async function() {
-    // Configuration
-    cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
-    });
-
-    // Upload an image
-    const uploadResult = await cloudinary.uploader
-        .upload(
-            "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg", {
-                public_id: "shoes",
-            }
-        )
-        .catch((error) => {
-            console.log(error);
-        });
-
-    console.log(uploadResult);
-
-    // Optimize delivery by resizing and applying auto-format and auto-quality
-    const optimizeUrl = cloudinary.url("shoes", {
-        fetch_format: "auto",
-        quality: "auto",
-    });
-
-    console.log(optimizeUrl);
-
-    // Transform the image: auto-crop to square aspect_ratio
-    const autoCropUrl = cloudinary.url("shoes", {
-        crop: "auto",
-        gravity: "auto",
-        width: 500,
-        height: 500,
-    });
-
-    console.log(autoCropUrl);
-})();
+export { uploadOnCloudinary, deletedFromCloudinary };
