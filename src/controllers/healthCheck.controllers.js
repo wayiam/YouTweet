ab245e75b238b3c9ccda4a75ef60c0d25ef59bbe
wayiam <sakshamsingh085@gmail.com>
2024-12-15 22:27:56 +0530

    Add controllers for comment, like, playlist, subscription, tweet, dashboard and video in controller directory

--- src/controllers/healthCheck.controllers.js
+++ src/controllers/healthCheck.controllers.js
@@ -1,10 +1,10 @@
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const healthCheck = asyncHandler(async (req, res) => {
-  return res
-    .status(200)
-    .json(new apiResponse(200, "OK", "Health Check Passed"));
+const healthCheck = asyncHandler(async(req, res) => {
+    return res
+        .status(200)
+        .json(new apiResponse(200, "OK", "Health Check Passed"));
 });
 
 export { healthCheck };
