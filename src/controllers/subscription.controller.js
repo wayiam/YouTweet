ab08e3908cf73bfccbb7c7a0520d0de3d9f04454
wayiam <sakshamsingh085@gmail.com>
2024-12-16 13:24:45 +0530

    Implement subscription toggling, fetching channel subscribers with aggregation, and retrieving subscribed channels with latest video details.

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,24 +1,189 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import { User } from "../models/user.model.js"
-import { Subscription } from "../models/subscription.model.js"
+import { User } from "../models/users.models.js"
+import { Subscription } from "../models/subscrition.models.js"
 import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js"
 
-
 const toggleSubscription = asyncHandler(async(req, res) => {
     const { channelId } = req.params
         // TODO: toggle subscription
+    if (!isValidObjectId(channelId)) {
+        throw new apiError(400, "ChannelId is not valid");
+    }
+
+    const isSubscribed = await Subscription.findOne({
+        subscriber: req.user && req.user._id,
+        channel: channelId,
+    })
+
+    if (isSubscribed) {
+        await Subscription.findByIdAndDelete(isSubscribed._id)
+
+        return res
+            .status(200)
+            .json(
+                new apiResponse(200, { subscribed: false }, "You have been subscribed/unsubscribed")
+            )
+    }
+
+    await Subscription.create({
+        subscriber: req.user && req.user._id,
+        channel: channelId,
+    })
+
+    return res
+        .status(200)
+        .json(
+            new apiResponse(
+                200, { subscribed: true }, "You have been subscribed/unsubscribed")
+        );
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async(req, res) => {
     const { channelId } = req.params
+
+    if (!isValidObjectId(channelId)) {
+        throw new apiError(400, "Invalid channelId");
+    }
+
+    const subscribers = await Subscription.aggregate([{
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscriber",
+                pipeline: [{
+                        $lookup: {
+                            from: "subscriptions",
+                            localField: "_id",
+                            foreignField: "channel",
+                            as: "subscribedToSubscriber",
+                        }
+                    },
+                    {
+                        $addFields: {
+                            subscribedToSubscriber: {
+                                $cond: {
+                                    if: {
+                                        $in: [channelId, "$subscribedToSubscriber.subscriber"],
+                                    },
+                                    then: true,
+                                    else: false,
+                                }
+                            },
+                            subscribersCount: {
+                                $size: "$subscribedToSubscriber"
+                            },
+                        },
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$subscriber",
+        },
+        {
+            $project: {
+                _id: 0,
+                subscriber: {
+                    _id: 1,
+                    username: 1,
+                    fullName: 1,
+                    "avatar.url": 1,
+                    subscribedToSubscriber: 1,
+                    subscribersCount: 1,
+                }
+            }
+        },
+    ]);
+
+    return res
+        .status(200)
+        .json(
+            new apiResponse(
+                200,
+                subscribers,
+                "Subscribers fetched successfully"
+            )
+        );
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async(req, res) => {
     const { subscriberId } = req.params
+
+    const subscribedChannels = await Subscription.aggregate([{
+            $match: {
+                subscriber: new mongoose.Types.ObjectId(subscriberId),
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "subscribedChannel",
+                pipeline: [{
+                        $lookup: {
+                            from: "videos",
+                            localField: "_id",
+                            foreignField: "owner",
+                            as: "videos",
+                        },
+                    },
+                    {
+                        $addFields: {
+                            latestVideo: {
+                                $last: "$videos",
+                            },
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $unwind: "$subscribedChannel",
+        },
+        {
+            $project: {
+                _id: 0,
+                subscribedChannel: {
+                    _id: 1,
+                    username: 1,
+                    fullName: 1,
+                    "avatar.url": 1,
+                    latestVideo: {
+                        _id: 1,
+                        "videoFile.url": 1,
+                        "thumbnail.url": 1,
+                        owner: 1,
+                        title: 1,
+                        description: 1,
+                        duration: 1,
+                        createdAt: 1,
+                        views: 1
+                    },
+                },
+            },
+        },
+    ]);
+
+    return res
+        .status(200)
+        .json(
+            new apiResponse(
+                200,
+                subscribedChannels,
+                "subscribed channels fetched successfully"
+            )
+        );
 })
 
 export {
